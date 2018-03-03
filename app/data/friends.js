92b644a03347f4f252ca471eb852069646b93976
Ryan Evans <Fatkid@Ryans-MacBook-Pro.local>
2018-03-02 18:44:18 -0800

    homepage and survey html done

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var FriendsList = [
+{
+    "Name": "yoda",
+    "photo": "Yoda",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      2,
+      3,
+      3,
+      4,
+      3
+    ]
+  },
+];
